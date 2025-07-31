2205becce51f78732ae7da3707ec1ca421593a7f
Anurag-xo <amnrag889@gmail.com>
2025-07-31 13:50:26 +0000

    create: nvim script

---
+++ scripts/nvim.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "🛠️ Setting up Neovim..."
+
+# Install Lazy.nvim (plugin manager)
+LAZY_DIR="$HOME/.local/share/nvim/site/pack/packer/start/Lazy.nvim"
+if [ ! -d "$LAZY_DIR" ]; then
+  git clone https://github.com/folke/lazy.nvim.git --branch=stable "$LAZY_DIR"
+fi
+
+# Install Mason, LSP, DAP, etc. via headless mode
+nvim --headless "+Lazy! sync" +qa
+
+echo "✅ Neovim plugins installed."
